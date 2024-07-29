ac8dc2e782547cebbdc73c954419a4345a83729d
diogorodriguees <diogorodrigueslife@gmail.com>
2024-07-28 21:49:16 -0300

    :sparkles: feat: change to accept image

--- server/server.rb
+++ server/server.rb
@@ -1,23 +1,71 @@
 require 'em-websocket'
 require 'json'
+require 'sinatra'
+require 'fileutils'
+
+set :public_folder, 'public'
+
+clients = {}
+typing_users = {}
 
 EM.run do
-  @clients = []
-
   EM::WebSocket.run(host: '0.0.0.0', port: 8080) do |ws|
     ws.onopen do |handshake|
-      puts "Client connected"
-      @clients << ws
+      user_id = handshake.query["user_id"]
+      group_id = handshake.query["group_id"]
+
+      clients[user_id] ||= {}
+      clients[user_id][group_id] ||= []
+      clients[user_id][group_id] << ws
+
+      ws.onmessage do |msg|
+        data = JSON.parse(msg)
+        if data["type"] == "text" || data["type"] == "image"
+          clients.each do |uid, groups|
+            if groups[group_id]
+              groups[group_id].each { |client| client.send msg }
+            end
+          end
+        elsif data["type"] == "typing"
+          typing_users[data["user_id"]] = data["is_typing"]
+          clients.each do |uid, groups|
+            if groups[group_id]
+              groups[group_id].each { |client| client.send msg }
+            end
+          end
+        end
+      end
+
+      ws.onclose do
+        clients[user_id][group_id].delete(ws)
+        clients[user_id].delete(group_id) if clients[user_id][group_id].empty?
+        clients.delete(user_id) if clients[user_id].empty?
+      end
+    end
+  end
+
+  Thread.new do
+    Sinatra::Application.run!
+  end
+end
+
+post '/upload' do
+  if params[:file] && params[:user_id] && params[:group_id]
+    filename = params[:file][:filename]
+    file = params[:file][:tempfile]
+
+    dir = "public/uploads/#{params[:group_id]}"
+    FileUtils.mkdir_p(dir) unless File.directory?(dir)
+    filepath = File.join(dir, filename)
+
+    File.open(filepath, 'wb') do |f|
+      f.write(file.read)
     end
 
-    ws.onmessage do |msg|
-      puts "Received message: #{msg}"
-      @clients.each { |client| client.send msg }
-    end
-
-    ws.onclose do
-      puts "Client disconnected"
-      @clients.delete(ws)
-    end
+    content_type :json
+    { status: 'success', url: "/uploads/#{params[:group_id]}/#{filename}" }.to_json
+  else
+    status 400
+    { status: 'error', message: 'Missing parameters' }.to_json
   end
 end
